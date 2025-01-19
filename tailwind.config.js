b2e2733acb5f9b714ca442d6170c2dff34f76231
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2025-01-19 16:59:03 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,21 @@ module.exports = {
                 deep_purple: {
                     "100_02": "var(--deep_purple_100_02)",
                     "100_33": "var(--deep_purple_100_33)",
-                    
-                }
+                    "a200_02": "var(--deep_purple_a200_02)",
+                    "a200_33": "var(--deep_purple_a200_33)",
+                    "a200_7f": "var(--deep_purple_a200_7f)",
+                },
+                gray: { 300: "var(--gray_300)", 400: "var(--gray_400)"},
+                high_contrast: "var(--high_contrast)",
+                light_blue: { 600: "var(--light_blue_600)"},
+                low_constrast: "var(--low_constrast)",
+                medium_constrast: "var(--medium_constrast)",
+                primary: "var(--primary)",
+                primary_shades: "var(--primary_shades)",
+                purple: { 600: "var(--purple_600)"},
+                white: "var(--white)",
+                black: { 600: "var(--back_600)"},
+                gray_shadow: "var(--gray_shadow)",
             }
         }
     }
