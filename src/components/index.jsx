2861c220044ba4d2e377c5431eaba06153d4a50f
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2025-01-22 09:09:00 +0100

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import { button } from "./button";
+import { checkBox } from "./checkbox";
+import { heading } from "./heading";
+import { img } from "./img";
+import { input } from "./input";
+import { selectbox } from "./selectbox";
+import { text } from "./text";
+
+export { button, checkBox, heading, img, input, selectbox, text };
